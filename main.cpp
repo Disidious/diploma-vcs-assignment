b07f077ab183ee32a16c0a08d964f4b9970d8d11
Disidious <fady.m.moussa@gmail.com>
2023-10-29 12:55:43 +0200

    Add getMin function

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 
 int getSum(std::vector<int> vec);
+int getMin(std::vector<int> vec);
 
 int main()
 {
     std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::cout << "Sum: " << getSum(vec);
+    std::cout << "Sum: " << getSum(vec) << "\n";
+    std::cout << "Min: " << getMin(vec);
 }
 
 int getSum(std::vector<int> vec)
@@ -18,4 +20,23 @@ int getSum(std::vector<int> vec)
     }
 
     return sum;
+}
+
+int getMin(std::vector<int> vec)
+{
+    if (vec.size() == 0)
+    {
+        return 0;
+    }
+
+    int min = vec[0];
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] < min)
+        {
+            min = vec[i];
+        }
+    }
+
+    return min;
 }
