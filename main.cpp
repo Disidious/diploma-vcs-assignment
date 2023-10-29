c6f430409ca3f6113b3bad0f8da73ddc14e7b4fc
Disidious <fady.m.moussa@gmail.com>
2023-10-29 12:42:13 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int getSum(std::vector<int> vec);
+
+int main()
+{
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << "Sum: " << getSum(vec);
+}
+
+int getSum(std::vector<int> vec)
+{
+    int sum = 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        sum += vec[i];
+    }
+
+    return sum;
+}
